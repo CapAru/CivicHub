6cf0c8cb65658d7da4607c34f96380aa4ccfd232
Arunava Chakrabarty <137680973+CapAru@users.noreply.github.com>
2025-01-11 19:52:09 +0530

    set up upvote functionality

--- src/elements/IssueBar.jsx
+++ src/elements/IssueBar.jsx
@@ -2,20 +2,36 @@ import { BiSolidUpvote } from "react-icons/bi";
 import { BiUpvote } from "react-icons/bi";
 import { CiWarning } from "react-icons/ci";
 import { CiMaximize1 } from "react-icons/ci";
+import { useState } from "react";
 
 export default function IssueBar() {
+    const [upvoted, setUpvoted] = useState(false);
+    const [upvotes, setUpvotes] = useState(0);
+    function changeUpvote(){
+        if(upvoted){
+            setUpvotes(upvotes-1);
+        }else{
+            setUpvotes(upvotes+1);
+        }
+        setUpvoted(!upvoted);
+    }
     return(
         <div className='my-2 w-3/4'>
             <div className='flex bg-[#0068ff] items-center justify-between py-4 px-8 rounded-2xl text-white'>
                 <div className='flex items-center'>
-                    <BiSolidUpvote className='mr-1 text-2xl'/>
+                    <div className="cursor-pointer flex items-center mr-2" onClick={changeUpvote}>
+                        {upvoted ? <BiSolidUpvote className='mr-1 text-2xl'/> : <BiUpvote className='mr-1 text-2xl'/>}
+                        <p className="text-lg">{upvotes}</p>
+                    </div>
                     <CiWarning className='mx-1 text-2xl'/>
                 </div>
                     <div className='flex flex-col items-center'>
                         <h3 className='font-bold text-xl'>Pothole on main street</h3>
                         <p>Submitted by: Arunava</p>
                     </div>
-                    <CiMaximize1 className='text-2xl'/>
+                    <div className='cursor-pointer'>
+                        <CiMaximize1 className='text-2xl'/>
+                    </div>
                 </div>
         </div>
     )
