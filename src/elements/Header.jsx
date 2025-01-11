a7d0ac6399f4aa0e0d75b8f252098fe209bff5d5
Amritesh Kumar <157412986+AmriteshKr24@users.noreply.github.com>
2025-01-11 20:49:24 +0530

    header

---
+++ src/elements/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header style={{ 
+            display: 'flex', 
+            justifyContent: 'space-between', 
+            alignItems: 'center', 
+            padding: '10px', 
+            position: 'fixed', 
+            top: 0, 
+            left: 0, 
+            width: '100%', 
+            backgroundColor: 'white', 
+            zIndex: 1000,
+            boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)'
+        }}>
+         
+            <div className="buttons" style={{ marginLeft: 'auto' }}>
+                <button className="login">Login</button>
+                <button className="signup">Sign Up</button>
+            </div>
+        </header>
+    );
+};
+document.body.style.marginTop = '60px';
+export default Header;
