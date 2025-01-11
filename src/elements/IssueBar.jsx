754e1fdb2f83590a5f5fab157958f9be0dadee35
Arunava Chakrabarty <137680973+CapAru@users.noreply.github.com>
2025-01-11 20:06:41 +0530

    Update IssueBar.jsx

--- src/elements/IssueBar.jsx
+++ src/elements/IssueBar.jsx
@@ -17,7 +17,7 @@ export default function IssueBar() {
     }
     return(
         <div className='my-2 w-3/4'>
-            <div className='flex bg-[#0068ff] items-center justify-between py-4 px-8 rounded-2xl text-white'>
+            <div className='flex bg-[#0068ff] items-center justify-between py-4 px-5 rounded-2xl text-white'>
                 <div className='flex items-center'>
                     <div className="cursor-pointer flex items-center mr-2 py-1 px-2 rounded-full hover:bg-[#2954c8] transition duration-200" onClick={changeUpvote}>
                         {upvoted ? <BiSolidUpvote className='mr-1 text-2xl'/> : <BiUpvote className='mr-1 text-2xl'/>}
@@ -25,14 +25,14 @@ export default function IssueBar() {
                     </div>
                     <CiWarning className='mx-1 text-2xl'/>
                 </div>
-                    <div className='flex flex-col items-center'>
-                        <h3 className='font-bold text-xl'>Pothole on main street</h3>
-                        <p>Submitted by: Arunava</p>
-                    </div>
-                    <div className='cursor-pointer'>
-                        <CiMaximize1 className='text-2xl'/>
-                    </div>
+                <div className='flex flex-col items-center'>
+                    <h3 className='font-bold text-xl'>Pothole on main street</h3>
+                    <p>Submitted by: Arunava</p>
                 </div>
+                <div className='cursor-pointer rounded-full hover:bg-[#2954c8] transition duration-200 py-1 px-2'>
+                    <CiMaximize1 className='text-2xl'/>
+                </div>
+            </div>
         </div>
     )
 }
