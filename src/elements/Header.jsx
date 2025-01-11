630824848b41797a0ffd281a019fc86bf3e51edf
Amritesh Kumar <157412986+AmriteshKr24@users.noreply.github.com>
2025-01-12 00:17:59 +0530

    submit

--- src/elements/Header.jsx
+++ src/elements/Header.jsx
@@ -13,16 +13,17 @@ const Header = () => {
             width: '100%', 
             backgroundColor: 'white', 
             zIndex: 1000,
-            boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)'
+            boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+            height: '60px', /* Fixed height for the header */
         }}>
-            <div className='mx-4'>
-                <a href="/" style={{ textDecoration: 'none', color: 'black' }}>
+            <div className='mx-6 text-2xl mr-2 font-bold'>
+                <a href="/" style={{ textDecoration: 'none', color: '#0068ff' }}>
                     <h1>Civic Hub</h1>
                 </a>
             </div>
-            <div class="hamburger"> 
-            <i class="fa-solid fa-bars"></i>
-        </div>
+            <div className="hamburger"> 
+                <i className="fa-solid fa-bars"></i>
+            </div>
             <div className="buttons" style={{ marginLeft: 'auto' }}>
                 <a href="/login" className="login"><button>Login</button></a>
                 <a href="/register" className="signup"><button>Sign Up</button></a>
