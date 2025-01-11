1aafc0130bddae028d59b5b04c87293c3225dedc
Arunava Chakrabarty <137680973+CapAru@users.noreply.github.com>
2025-01-11 19:58:56 +0530

    finalized frontend of issues

--- src/elements/IssueBar.jsx
+++ src/elements/IssueBar.jsx
@@ -19,7 +19,7 @@ export default function IssueBar() {
         <div className='my-2 w-3/4'>
             <div className='flex bg-[#0068ff] items-center justify-between py-4 px-8 rounded-2xl text-white'>
                 <div className='flex items-center'>
-                    <div className="cursor-pointer flex items-center mr-2" onClick={changeUpvote}>
+                    <div className="cursor-pointer flex items-center mr-2 py-1 px-2 rounded-full hover:bg-[#2954c8] transition duration-200" onClick={changeUpvote}>
                         {upvoted ? <BiSolidUpvote className='mr-1 text-2xl'/> : <BiUpvote className='mr-1 text-2xl'/>}
                         <p className="text-lg">{upvotes}</p>
                     </div>
