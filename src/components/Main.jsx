b4525deb1ce634ba42e34986bac51f826c65d558
Arunava Chakrabarty <137680973+CapAru@users.noreply.github.com>
2025-01-11 19:16:10 +0530

    ss

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,21 +6,29 @@ import { CiMaximize1 } from "react-icons/ci";
 export default function Main(){
     return(
         <>
-            <div className='flex justify-evenly'>
-                <div className='mx-4 p-10 rounded-2xl border-8 border-[#44546a] '>
-                    <h2 className='mb-10'>Active Issues</h2>
-                    <div className='flex bg-[#0068ff] items-center justify-between p-4 rounded-2xl text-white'>
-                        <BiSolidUpvote />
-                        <CiWarning />
-                        <div>
-                            <h3>Issue Title</h3>
-                            <p>Issue Description</p>
-                        </div>
-                        <CiMaximize1 />
+            <div className='flex flex-col items-center my-3'>
+                <div className='flex justify-evenly w-3/4 border-b-4 p-3'>
+                    <div className='mx-4 p-1'>
+                        <h2>Active Issues</h2>
+                        
+                    </div>
+                    <div className='mx-4 p-1'>
+                        <h2>Resolved Issues</h2>
                     </div>
                 </div>
-                <div className='mx-4 p-10 rounded-2xl border-8 border-[#44546a] '>
-                    <h2>Resolved Issues</h2>
+                
+                <div className='my-4 w-3/4'>
+                    <div className='flex bg-[#0068ff] items-center justify-between py-4 px-8 rounded-2xl text-white'>
+                        <div className='flex items-center'>
+                            <BiSolidUpvote className='mx-1'/>
+                            <CiWarning className='mx-1'/>
+                        </div>
+                            <div className='flex flex-col items-center'>
+                                <h3 className='font-bold text-xl'>Pothole on main street</h3>
+                                <p>Submitted by: Arunava</p>
+                            </div>
+                            <CiMaximize1 className='mx-2'/>
+                        </div>
                 </div>
             </div> 
         </>
